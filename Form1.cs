9621129a33082cf0f62439a6352616a6f41b22ad
IbrahimM9 <511468@vistacollege.nl>
2025-03-02 12:10:49 +0100

    #Comments

--- Form1.cs
+++ Form1.cs
@@ -21,6 +21,7 @@ namespace MPA
         }
 
         private void siticoneButton1_Click(object sender, EventArgs e)
+        // Dit zorgt ervoor dat het programma wordt afgesloten
         {
             Application.Exit();
         }
@@ -35,10 +36,14 @@ namespace MPA
 
         private void siticoneButton2_Click(object sender, EventArgs e)
         {
+            // Waardes worden aan processStartInfo toegekent
+
             ProcessStartInfo processStartInfo = new ProcessStartInfo();
             processStartInfo.FileName = "powershell.exe";
             processStartInfo.Arguments = "chkdsk c: /F /R /X";
             processStartInfo.Verb = "RunAs";
+            
+            // Het process wordt gestart
             Process.Start(processStartInfo);
         }
 
@@ -62,12 +67,15 @@ namespace MPA
 
         private void siticoneButton3_Click(object sender, EventArgs e)
         {         
+            // Dit pakt de %temp% van de gebruiker en verwijdert het vervolgens
             try 
             {
                 string tempPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "Temp");
 
                 Directory.Delete(tempPath, true);
             }
+            
+            // Aan de hand van de exception zal de gebruiker een melding krijgen
             catch (IOException) 
             {
                 MessageBox.Show("Sommige bestanden zijn nog in gebruik, deze zijn niet verwijderd.", "Bestanden in gebruik", MessageBoxButtons.OK);
@@ -85,12 +93,20 @@ namespace MPA
 
         private async void siticoneButton5_Click_1(object sender, EventArgs e)
         {
+           // Waardes worden aan processStartInfo gegeven.
+     
             ProcessStartInfo processStartInfo = new ProcessStartInfo();
             processStartInfo.FileName = "powershell.exe";
             processStartInfo.Arguments = "Clear-RecycleBin -Force Start-Sleep \r\n-Seconds 2 \r\nClear-RecycleBin -Force";
             processStartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+            
+            // Process wordt gestart
             Process.Start(processStartInfo);
+            
+            // Wacht 2 seconden voordat de melding wordt weergegeven
             await Task.Delay(2000);
+            
+            // Laat een melding zien dat de prullenbak is leeggemaakt
             MessageBox.Show("Prullenbak is leeggemaakt.");
             
         }
